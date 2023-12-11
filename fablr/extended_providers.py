179a8102cb4c02031d9bbb40db089e64eba01361
Jesus Caro <jmcaro2@icloud.com>
2023-12-11 02:54:16 +0000

    finish sample df and list providers

--- fablr/extended_providers.py
+++ fablr/extended_providers.py
@@ -1,10 +1,10 @@
-from faker.providers import BaseProvider, DynamicProvider
+from faker.providers import BaseProvider
 
 
 class sample_dataframe_provider(BaseProvider):
     def __init__(self, faker):
-       super().__init__(faker)
-       self.generated_values = set()
+        super().__init__(faker)
+        self.generated_values = set()
 
     def sample_dataframe_provider_clear(self):
         self.generated_values = set()
@@ -17,27 +17,25 @@ class sample_dataframe_provider(BaseProvider):
         else:
             raise ValueError("unique must be a boolean")
 
-
-    def to_many_value(self,df, column):
+    def to_many_value(self, df, column):
         elements = df[column].to_list()
-        return self.random_elements(elements, length=1, unique = False)[0]
+        return self.random_elements(elements, length=1, unique=False)[0]
 
     def sample_and_append_to_set(self, df, column):
         if len(self.generated_values) == len(df[column]):
-            error_string = "Sampling rate reached, you are attempting to sample more times than the number of rows of the dataframe you are referencing."
+            error_string = "Sampling rate reached you are attempting to sample more times than the number of rows of the dataframe you are referencing."
             raise ValueError(error_string)
-        value = df[column][self.random_int(min=0,
-                                              max=len(df[column])-1)]
+        value = df[column][self.random_int(min=0, max=len(df[column]) - 1)]
         while value in self.generated_values:
-           value = df[column][self.random_int(min=0,
-                                              max=len(df[column])-1)]
+            value = df[column][self.random_int(min=0, max=len(df[column]) - 1)]
         self.generated_values.add(value)
         return value
 
+
 class sample_list_provider(BaseProvider):
     def __init__(self, faker):
-       super().__init__(faker)
-       self.generated_values = set()
+        super().__init__(faker)
+        self.generated_values = set()
 
     def sample_list_provider_clear(self):
         self.generated_values = set()
@@ -57,6 +55,6 @@ class sample_list_provider(BaseProvider):
             raise ValueError(error_string)
         value = self.random_element(list)
         while value in self.generated_values:
-              value = self.random_element(list)
+            value = self.random_element(list)
         self.generated_values.add(value)
         return value
